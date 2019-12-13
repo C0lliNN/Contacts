635973bfbbd0d31de42d6d63d6118230efeece23
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2019-12-13 08:33:39 -0300

    Updating query.sql - Translate to English and set email field as optional

--- query.sql
+++ query.sql
@@ -1,24 +1,18 @@
 
 
-create database Contatos;
+create database Contacts;
 
-use Contatos;
+use Contacts;
 
-create table Contatos(
-	idContato int primary key auto_increment,
-	nome varchar(50) not null unique,
-	sexo enum('Masculino','Feminino'),
-	numero varchar(20) not null unique,
-	email varchar(50) not null unique,
-	descricao varchar(250)
+create table Contacts(
+	idContact int primary key auto_increment,
+	name varchar(50) not null unique,
+	gender enum('Male','Female'),
+	phoneNumber varchar(20) not null unique,
+	email varchar(50),
+    description varchar(250)
 );
 
-/* Regras de Negocio
-
-1 - Nao pode existir dois contatos com o mesmo nome
-2 - Nao pode existir dois contatos com o mesmo numero
-3 - Nao pode existir dois contatos com o mesmo email
-4 - O campo descricao e opcional */
 
 
 
